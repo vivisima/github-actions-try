7907096aab6eaa15ed8a04c4aa4376f3beb79251
viviana.camacho <viviana.camacho@jalasoft.com>
2020-02-13 00:16:33 -0400

    Initial commit

---
+++ src/main/groovy/com/vivisima/githubactionstry/GithubActionsTryApplication.groovy
@@ -0,0 +1,13 @@
+package com.vivisima.githubactionstry
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class GithubActionsTryApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(GithubActionsTryApplication, args)
+	}
+
+}
---
+++ src/test/groovy/com/vivisima/githubactionstry/GithubActionsTryApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.vivisima.githubactionstry
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GithubActionsTryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
